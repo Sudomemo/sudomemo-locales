56cbc3dbac7d07113d24129f1acce15bfe78792d
sudofox <apburk@gmail.com>
2020-09-17 17:32:12 -0400

    Add ignores

--- find_problems.sh
+++ find_problems.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 echo "# sudomemo-locales Problem Finder"
 echo "Scans for problems as well as untranslated (or identical) strings"
 echo
@@ -17,50 +19,50 @@ fi
 for lang in $(echo ??_?? | tr ' ' '\n' | grep $LANGFILTER); do
     echo "## $lang"
     echo
-    for domain in $(ls en_US/LC_MESSAGES/*.po | xargs -L1 basename); do
+    for domain in $(ls en_US/LC_MESSAGES/*.po | xargs -L1 -I{} basename {} .po); do
 
         # Missing locale file?
-        if [ ! -f $lang/LC_MESSAGES/$domain ]; then
-            echo "Error: $lang/LC_MESSAGES/$domain is missing"
+        if [ ! -f $lang/LC_MESSAGES/$domain.po ]; then
+            echo "Error: $lang/LC_MESSAGES/$domain.po is missing"
             exit 1
         fi
 
         # Wrong text encoding?
 
-        CHECK_ENCODING=$(file $lang/LC_MESSAGES/$domain | egrep -v "(UTF-8|ASCII|empty)")
+        CHECK_ENCODING=$(file $lang/LC_MESSAGES/$domain.po | egrep -v "(UTF-8|ASCII|empty)")
 
         if [ ! -z "$CHECK_ENCODING" ]; then
-            echo "$lang/LC_MESSAGES/$domain has wrong encoding: should be detected as UTF-8, ASCII, or empty"
-            file $lang/LC_MESSAGES/$domain
+            echo "$lang/LC_MESSAGES/$domain.po has wrong encoding: should be detected as UTF-8, ASCII, or empty"
+            file $lang/LC_MESSAGES/$domain.po
             exit 1
         fi
 
         # Invalid?
 
-        msgfmt --check-format $lang/LC_MESSAGES/$domain -o - >/dev/null
+        msgfmt --check-format $lang/LC_MESSAGES/$domain.po -o - >/dev/null
 
         if [ $? -ne 0 ]; then
-            echo "Check the formatting for $lang/LC_MESSAGES/$domain"
+            echo "Check the formatting for $lang/LC_MESSAGES/$domain.po"
             exit 1
         fi
 
         # Missing/extra strings?
 
-        ENG_MSGID_LIST=$(grep -Po "^msgid..\K.+?(?=\")" en_US/LC_MESSAGES/$domain | sort | uniq)
-        NEW_MSGID_LIST=$(grep -Po "^msgid..\K.+?(?=\")" $lang/LC_MESSAGES/$domain | sort | uniq)
+        ENG_MSGID_LIST=$(grep -Po "^msgid..\K.+?(?=\")" en_US/LC_MESSAGES/$domain.po | sort | uniq)
+        NEW_MSGID_LIST=$(grep -Po "^msgid..\K.+?(?=\")" $lang/LC_MESSAGES/$domain.po | sort | uniq)
 
         COMPARE_MISSING=$(comm -23 <(echo "$ENG_MSGID_LIST") <(echo "$NEW_MSGID_LIST"))
         COMPARE_EXTRA=$(comm -23 <(echo "$NEW_MSGID_LIST") <(echo "$ENG_MSGID_LIST"))
 
         if [ ! -z "$COMPARE_MISSING" ]; then
-            echo "$lang/LC_MESSAGES/$domain is missing msgid's present in en_US/LC_MESSAGES/$domain :"
+            echo "$lang/LC_MESSAGES/$domain.po is missing msgid's present in en_US/LC_MESSAGES/$domain.po :"
             echo "$COMPARE_MISSING"
             echo "Please fix before continuing."
             exit 1
         fi
 
         if [ ! -z "$COMPARE_EXTRA" ]; then
-            echo "$lang/LC_MESSAGES/$domain has extra msgid's compared to en_US/LC_MESSAGES/$domain :"
+            echo "$lang/LC_MESSAGES/$domain.po has extra msgid's compared to en_US/LC_MESSAGES/$domain.po :"
             echo "$COMPARE_EXTRA"
             echo "Please fix before continuing."
             exit 1
@@ -69,11 +71,11 @@ for lang in $(echo ??_?? | tr ' ' '\n' | grep $LANGFILTER); do
         # Number of lines differs?
         # Disabled for now
 
-        # ENG_LINE_COUNT=$(wc -l < en_US/LC_MESSAGES/$domain)
-        # NEW_LINE_COUNT=$(wc -l < $lang/LC_MESSAGES/$domain)
+        # ENG_LINE_COUNT=$(wc -l < en_US/LC_MESSAGES/$domain.po)
+        # NEW_LINE_COUNT=$(wc -l < $lang/LC_MESSAGES/$domain.po)
 
         # if [ "$ENG_LINE_COUNT" -ne "$NEW_LINE_COUNT" ]; then
-        #     echo "Line count for $lang/LC_MESSAGES/$domain different from en_US/LC_MESSAGES/$domain"
+        #     echo "Line count for $lang/LC_MESSAGES/$domain.po different from en_US/LC_MESSAGES/$domain.po"
         #     echo "Check for missing/extra newlines inside and at the top/bottom of the file"
         #     echo "en_US: $ENG_LINE_COUNT"
         #     echo "$lang: $NEW_LINE_COUNT"
@@ -85,13 +87,20 @@ for lang in $(echo ??_?? | tr ' ' '\n' | grep $LANGFILTER); do
         SKIP_DIFF_REGEX="en_(AU|GB|US)"
 
         if [[ $lang =~ $SKIP_DIFF_REGEX ]]; then
-            echo "Skipping comparison for $domain: $lang is an English variant"
+            echo "Skipping comparison for $domain.po: $lang is an English variant"
             continue
         fi
 
-        RESULTS=$(diff --unchanged-line-format='%L' --old-line-format='' --new-line-format='' en_US/LC_MESSAGES/$domain $lang/LC_MESSAGES/$domain | sed '/^$/d' | grep -B1 msgstr | grep -Po "^msgid..\K.+?(?=\")" | awk '{print "- " $1}')
+        RESULTS=$(diff --unchanged-line-format='%L' --old-line-format='' --new-line-format='' en_US/LC_MESSAGES/$domain.po $lang/LC_MESSAGES/$domain.po | sed '/^$/d' | grep -B1 msgstr | grep -Po "^msgid..\K.+?(?=\")" | sort)
+
+        # check ignore list
+        if [ -f $DIR/ignores/$lang/$domain.txt ]; then
+            PRUNE=$(comm -23 <(echo "$RESULTS") <(sort $DIR/ignores/$lang/$domain.txt | uniq))
+            RESULTS=$PRUNE
+        fi
+
         if [ ! -z "$RESULTS" ]; then
-            echo "$lang/LC_MESSAGES/$domain:"
+            echo "$lang/LC_MESSAGES/$domain.po:"
             echo "$RESULTS"
             echo
         fi
