5db0ce592d5635223fc68f97c368d384c04154c9
sudofox <apburk@gmail.com>
2020-09-17 13:33:55 -0400

    Ready for September Translation Roundup

--- clean_all.sh
+++ clean_all.sh
@@ -4,7 +4,7 @@ for domain in $(ls en_US/LC_MESSAGES/*.po|xargs -L1 basename); do
     for lang in $(echo ??_??); do
 				# Don't sort unless we edit in the -s flag
 				# This way we don't cause massive merge conflicts all the time
-				# msgcat --no-wrap -s en_US/LC_MESSAGES/login.po -o en_US/LC_MESSAGES/login.po
-				msgcat --no-wrap $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
+				# msgcat -s --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
+				msgcat --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
     done
 done
