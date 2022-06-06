f7d3ef70a36f3415578ff993415cc85d5edce945
Austin Burk <apburk@sudomemo.net>
2022-06-06 19:26:34 -0400

    Support token final (hopefully)

--- clean_all.sh
+++ clean_all.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-for domain in $(ls en_US/LC_MESSAGES/*.po|xargs -L1 basename); do
+# if a locale file name is passed as an argument, use it
+if [ $# -eq 1 ]; then
+	LOCALE_FILE=$1
+else
+	LOCALE_FILE="*"
+fi
+
+DO_SORT=false
+
+for domain in $(ls en_US/LC_MESSAGES/$LOCALE_FILE.po|xargs -L1 basename); do
     for lang in $(echo ??_??); do
-				# Don't sort unless we edit in the -s flag
-				# This way we don't cause massive merge conflicts all the time
-				# msgcat -s --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
-				msgcat --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
+		# Don't sort unless we edit in the -s flag
+		# This way we don't cause massive merge conflicts all the time
+		if $DO_SORT; then
+			echo "Sorting"
+			msgcat -s --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
+		else
+			msgcat --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
+		fi
     done
 done
