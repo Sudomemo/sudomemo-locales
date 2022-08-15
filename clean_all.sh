5f6cfd6801500c0edbd5f1cdc28ee4ee73736924
Austin Burk <apburk@sudomemo.net>
2022-08-14 22:54:37 -0400

    sudomemo radio tickets/trophies

--- clean_all.sh
+++ clean_all.sh
@@ -14,9 +14,10 @@ for domain in $(ls en_US/LC_MESSAGES/$LOCALE_FILE.po|xargs -L1 basename); do
 		# Don't sort unless we edit in the -s flag
 		# This way we don't cause massive merge conflicts all the time
 		if $DO_SORT; then
-			echo "Sorting"
+			echo "Cleaning and sorting $lang/$domain"
 			msgcat -s --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
 		else
+			echo "Cleaning $lang/$domain"
 			msgcat --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
 		fi
     done
