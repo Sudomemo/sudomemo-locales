b140e7ecd2a0b9da7b7da61106721395435411fe
Austin Burk <apburk@gmail.com>
2022-09-06 14:47:23 -0400

    Modify archive+theatre for phrasing test, add GH action

--- clean_all.sh
+++ clean_all.sh
@@ -7,17 +7,21 @@ else
 	LOCALE_FILE="*"
 fi
 
-DO_SORT=false
+if [ "$DO_SORT" = "true" ]; then
+	DO_SORT=true
+else
+	DO_SORT=false
+fi
 
 for domain in $(ls en_US/LC_MESSAGES/$LOCALE_FILE.po|xargs -L1 basename); do
     for lang in $(echo ??_??); do
 		# Don't sort unless we edit in the -s flag
 		# This way we don't cause massive merge conflicts all the time
 		if $DO_SORT; then
-			echo "Cleaning and sorting $lang/$domain"
+			echo "Cleaning and sorting $lang/LC_MESSAGES/$domain"
 			msgcat -s --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
 		else
-			echo "Cleaning $lang/$domain"
+			echo "Cleaning $lang/LC_MESSAGES/$domain"
 			msgcat --to-code=UTF-8 --no-wrap --lang=$lang $lang/LC_MESSAGES/$domain -o $lang/LC_MESSAGES/$domain
 		fi
     done
