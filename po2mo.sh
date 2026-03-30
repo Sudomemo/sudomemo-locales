70a564bfe1a05783956ab5f560b3165db5a38a76
Austin Burk <apburk@gmail.com>
2026-03-30 12:37:45 -0400

    Update po2mo.sh to allow scoping of recompilation

--- po2mo.sh
+++ po2mo.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-# Remove Gettext .mo files and regenerate them from the .po files
-for i in $(find . -name \*.po|sed 's/\.po//g'); do
-	if [ -f "$i.mo" ]; then
-		rm $i.mo;
-		msgfmt $i.po -o $i.mo
-		echo $i;
-	elif [ -f "$i.po" ]; then
-		msgfmt $i.po -o $i.mo
-	fi
-done
 
-echo "Done! Make sure to update file permissions and reload Apache."
+# Remove Gettext .mo files and regenerate them from the .po files
+textdomain="$1"
+compiled_count=0
+
+if [ -n "$textdomain" ]; then
+	po_files=$(find . -name "${textdomain}.po")
+	if [ -z "$po_files" ]; then
+		echo "No .po files found for textdomain: $textdomain"
+		exit 1
+	fi
+else
+	po_files=$(find . -name '*.po')
+fi
+
+while IFS= read -r po_file; do
+	[ -z "$po_file" ] && continue
+
+	base="${po_file%.po}"
+	if [ -f "$base.mo" ]; then
+		rm "$base.mo"
+	fi
+
+	msgfmt "$po_file" -o "$base.mo"
+	echo "$base"
+	compiled_count=$((compiled_count + 1))
+done <<< "$po_files"
+
+if [ -n "$textdomain" ]; then
+	echo "Done! Recompiled $compiled_count textdomain file(s) for '$textdomain'."
+else
+	echo "Done! Recompiled $compiled_count textdomain file(s)."
+fi
+
+echo "Make sure to update file permissions and reload Apache."
